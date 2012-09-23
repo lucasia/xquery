9720e0c0e2583ab3eb74d370051e8736e72dc0cb
lucasia <ian.lucas@gmail.com>
2012-09-23 13:09:20 +0800

    changed to param test cases

--- src/test/java/XQueryParamTestSuite.java
+++ src/test/java/XQueryParamTestSuite.java
@@ -3,38 +3,41 @@ import junit.framework.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
-import org.junit.runners.model.FrameworkMethod;
+import uk.ac.lkl.common.util.testing.LabelledParameterized;
 
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.Collection;
 
 /**
  * User: ialucas
  * Test to ensure the XQuery unit test works as expected
  */
-@RunWith(value = Parameterized.class)
+@RunWith(value = LabelledParameterized.class)
 public class XQueryParamTestSuite {
 
     private String xqFilePath;
+    private String label;
 
-    public XQueryParamTestSuite(String xqFilePath) {
+    public XQueryParamTestSuite(String label, String xqFilePath) {
+        this.label = label;
         this.xqFilePath = xqFilePath;
     }
 
+
     @Parameterized.Parameters
-    public static Collection<Object[]> data() {
-        Object[][] data = new Object[][] {
-                                    { "src/test/xquery/sample/message-business-logic-test.xqy" },
-                                    { "src/test/xquery/reference/conform-test.xqy" },
-                                    { "src/test/xquery/reference/format-test.xqy" },
-                                    { "src/test/xquery/reference/functional-test.xqy" }
-        };
+    public static Collection<Object[]> getParameters() {
+        Collection<Object[]> parameters = new ArrayList<Object[]>();
 
-        return Arrays.asList(data);
+        parameters.add(new Object[]{"message-business-logic-test.xqy", "src/test/xquery/sample/message-business-logic-test.xqy"});
+        parameters.add(new Object[]{"conform-test.xqy", "src/test/xquery/reference/conform-test.xqy"});
+        parameters.add(new Object[]{"format-test.xqy", "src/test/xquery/reference/format-test.xqy"});
+        parameters.add(new Object[]{"functional-test.xqy", "src/test/xquery/reference/functional-test.xqy"});
+
+        return parameters;
+
     }
-
 
     @Test
     public void testXQuery() throws Exception {
